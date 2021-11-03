2a243c66966926e5f29ace82423b011ca58debd7
pepemax <xdpepemaxdx@gmail.com>
2021-11-03 19:26:56 +0100

    Add AP for button version

--- GeekLoverWithButton/GeekLover.cpp
+++ GeekLoverWithButton/GeekLover.cpp
@@ -37,20 +37,42 @@ WiFiUDP ntpUDP;
 
 NTPClient temps(ntpUDP, "fr.pool.ntp.org", 3600, 1000); //3600 = TimeOffset for my country (France) and 1000 = frequency of update time 
 
+//callback notifying us of the need to save config
+void saveConfigCallback()
+{
+    
+  ESP.restart();
+
+}
+
 void setup()
 {
 
+  // put your setup code here, to run once:
+  WiFi.mode(WIFI_STA); // explicitly set mode, esp defaults to STA+AP
+  // it is a good practice to make sure your code sets wifi mode how you want it.
+
+  // put your setup code here, to run once:
   Serial.begin(115200);
 
+  //WiFiManager, Local intialization. Once its business is done, there is no need to keep it around
+  WiFiManager wm;
+
+  // reset settings - wipe stored credentials for testing
+  // these are stored by the esp library
+  // wm.resetSettings();
+
+  // Automatically connect using saved credentials,
+  // if connection fails, it starts an access point with the specified name ( "AutoConnectAP"),
+  // if empty will auto generate SSID, if password is blank it will be anonymous AP (wm.autoConnect())
+  // then goes into a blocking loop awaiting configuration and will return success result
+  wm.setSaveConfigCallback(saveConfigCallback);
+
+  // res = wm.autoConnect(); // auto generated AP name from chipid
+  // res = wm.autoConnect("AutoConnectAP"); // anonymous ap
+  wm.autoConnect("GeekLover", "!GeekLover!"); // password protected ap
+
   temps.setUpdateInterval(500);
-
-  WiFi.begin(ssid, mdp);
-
-  while (WiFi.status() != WL_CONNECTED)
-  {
-    Serial.print(".");
-    delay(100);
-  }
 
   temps.begin();
 
